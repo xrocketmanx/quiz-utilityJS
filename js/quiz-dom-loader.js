6a212a6c200808a78f7d4e7b6878fbe33a456854
Alexander <xrocketmanx007@gmail.com>
2016-04-27 11:52:58 +0300

    added simple navigation and question load

--- js/quiz-dom-loader.js
+++ js/quiz-dom-loader.js
@@ -9,37 +9,73 @@ function QuizDOMLoader() {
 	var FORM_CLASS = 'qu-form';
 	var NAV_CLASS = 'qu-nav';
 	var MAIN_CLASS = 'qu-main';
+	var QUESTION_CLASS = 'qu-question';
 	var BUTTON_CLASS = 'qu-btn-';
 	var BUTTON_TYPES = ['previous', 'skip', 'answer', 'next', 'end'];
 
+	/**
+	 * Loads DOM objects for quiz except questions
+	 * @param  {Number} questionsCount for navigation bar
+	 */
 	this.loadQuizDOM = function(questionsCount) {
 		var form = document.getElementsByClassName(FORM_CLASS)[0];
 		loadNav(form, questionsCount);
 		loadMain(form);
 		loadButtons(form);
-	}
+	};
 
+	/**
+	 * Loads Question in DOM
+	 * @param  {Question} question 
+	 */
 	this.loadQuestion = function(question) {
+		var main = document.getElementsByClassName(MAIN_CLASS)[0];
+		main.innerHTML = '';
+		var p = document.createElement('p');
+		p.classList.add(QUESTION_CLASS);
+		p.appendChild(document.createTextNode(question.text));
+		main.appendChild(p);
+		switch(question.type) {
+			case 'multiple': {
+				loadOptions(main, question.options, 'checkbox');
+				break;
+			}
+			case 'single': {
+				loadOptions(main, question.options, 'radio');
+				break;
+			}
+		}
+	};
 
+	function loadOptions(main, options, type) {
+		for (var i = 0; i < options.length; i++) {
+			var input = document.createElement('input');
+			input.type = type;
+			input.name = 'answer';
+			input.id = 'answer' + i;
+			var label = document.createElement('label');
+			label.htmlFor = 'answer' + i;
+			label.appendChild(document.createTextNode(options[i]));
+			main.appendChild(input);
+			main.appendChild(label);
+		}
 	}
 
 	function loadNav(form, questionsCount) {
-		if (document.getElementsByClassName(NAV_CLASS).length > 0) return;
+		if (document.querySelector('.' + NAV_CLASS)) return;
 
-		var nav = document.createElement('nav');
-		nav.classList.add(NAV_CLASS);
 		var ul = document.createElement('ul');
-		nav.appendChild(ul);
+		ul.classList.add(NAV_CLASS);
 		for (var i = 0; i < questionsCount; i++) {
 			var li = document.createElement('li');
 			li.appendChild(document.createTextNode(i + 1));
 			ul.appendChild(li);
 		}
-		form.appendChild(nav);
+		form.appendChild(ul);
 	}
 
 	function loadMain(form) {
-		if (document.getElementsByClassName(MAIN_CLASS).length > 0) return;
+		if (document.querySelector('.' + MAIN_CLASS)) return;
 
 		var main = document.createElement('div');
 		main.classList.add(MAIN_CLASS);
--- js/quiz.js
+++ js/quiz.js
@@ -14,16 +14,47 @@ function Quiz(questions, time, callback) {
 	var quizNavigator = new QuizNavigator(questions);
 	var quizDOMLoader = new QuizDOMLoader();
 
+	/**
+	 * Loads quiz DOM and bind handlers
+	 */
 	this.loadQuiz = function() {
 		quizDOMLoader.loadQuizDOM(questions.length);
 		bindHandlers();
 	};
 
+	/**
+	 * Starts quiz timer and loads first question
+	 */
 	this.startQuiz = function() {
-
+		quizDOMLoader.loadQuestion(quizNavigator.getCurrent());
 	};
 
 	function bindHandlers() {
+		var nextBtn = document.querySelector('.qu-btn-next');
+		nextBtn.addEventListener('click', function() {
+			quizNavigator.next();
+			quizDOMLoader.loadQuestion(quizNavigator.getCurrent());
+		}); 
 
+		var skipBtn = document.querySelector('.qu-btn-skip');
+		skipBtn.addEventListener('click', function() {
+			quizNavigator.nextUnanswered();
+			quizDOMLoader.loadQuestion(quizNavigator.getCurrent());
+		}); 
+
+		var prevBtn = document.querySelector('.qu-btn-previous');
+		prevBtn.addEventListener('click', function() {
+			quizNavigator.prev();
+			quizDOMLoader.loadQuestion(quizNavigator.getCurrent());
+		});
+
+		var nav = document.querySelector('.qu-nav'); 
+		var onNavClick = function() {
+			quizNavigator.setCursor(this.innerHTML - 1);
+			quizDOMLoader.loadQuestion(quizNavigator.getCurrent());
+		};
+		for (var i = 0; i < nav.childNodes.length; i++) {
+			nav.childNodes[i].addEventListener('click', onNavClick);
+		}
 	}
 }
